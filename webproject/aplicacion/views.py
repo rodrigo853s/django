243c9e848f6fa46bff8e36b28c860e377896ef63
rodrigo853s <nsswitch.conf@gmail.com>
2025-03-07 19:43:00 +0100

    v2

--- webproject/aplicacion/views.py
+++ webproject/aplicacion/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#   return HttpResponse("¡¡¡ Mi primera página Django !!!")
+
 def index(request):
-    return HttpResponse("¡¡¡ Mi primera página Django !!!")
+   return render(request, 'myapp/indice.html')
 
 def metodoViernes(request):
     return HttpResponse("Thanks God it's Friday !!!")
 
 # y nos vamos a aplicacion/urls.py y lo rellenamos, que está vacío
+
+
 
 
